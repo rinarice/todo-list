411e42b6802254f6978b1c40451e807a339e2299
Kateryna <kat.rin47u@gmail.com>
2024-12-03 16:58:01 +0000

    feat: add tags and deadline fields to Task form

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,14 +1,27 @@
 from django import forms
-from tasks.models import Task
+from tasks.models import Task, Tag
+
 
 class TaskForm(forms.ModelForm):
     status = forms.ChoiceField(
         choices=[(True, "Done"), (False, "Not Done")],
         widget=forms.RadioSelect,
         label="Task Status",
-        help_text="Select 'Done' if the task is completed or 'Not Done' otherwise.",
+        help_text="Select 'Done' if the task is completed or 'Not Done' "
+                  "otherwise.",
+    )
+
+    tags = forms.ModelMultipleChoiceField(
+        queryset=Tag.objects.all(),
+        widget=forms.CheckboxSelectMultiple(),
+        required=False,
+    )
+
+    deadline = forms.DateTimeField(
+        widget=forms.DateTimeInput(attrs={"type": "datetime-local"}),
+        required=False,
     )
 
     class Meta:
         model = Task
-        fields = "__all__"
+        fields = ["content", "status", "tags", "deadline"]
